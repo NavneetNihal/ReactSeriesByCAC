81e32ea7ef3741e44a9fc39f73c40f270850e80a
Navneet Nihal <lakranihal0070@gmail.com>
2025-12-22 21:44:16 +0530

    #

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,7 +12,10 @@ function App() {
   const addValue = () => {
     // counter = counter + 1
     if (counter < 20) {
-      setCounter(counter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
     }
     // console.log("clicked", counter);
   }
