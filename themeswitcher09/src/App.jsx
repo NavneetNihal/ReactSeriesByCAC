7b45c8fa18c5cc9a3de7e4e3fb667560d9fe1193
Navneet Nihal <lakranihal0070@gmail.com>
2026-01-17 23:27:03 +0530

    Theme Mode Switcher

--- themeswitcher09/src/App.jsx
+++ themeswitcher09/src/App.jsx
@@ -6,25 +6,31 @@ import Card from './Components/card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
+ //  Serial NO - 3  if the global object has the info in memory that it is lighTheme than we are running a function which set or update themeMode by setThemeMode method and store this "light" as string inside themeMode.
 
   const lightTheme = () => {
     setThemeMode("light")
   }
-
+//  Serial NO - 4 same happens with darkTheme if global object has memory that it is darkTheme then thia method runs and it store "dark" as string in themeMode.
   const darkTheme = () => {
     setThemeMode("dark")
   }
 
   // Actual change in theme
 
+ // Serial NO - 5  Meanwhile useEffect runs and clear the html doesn't matter it is light or dark it just clears it so we can set the theme coz if we dont clear it and themeMode sets it as dark or it has already light class then it will clash.
+
   useEffect(() => {
     const htmlClasslist = document.querySelector('html').classList;
     htmlClasslist.remove("light", "dark");
+    
+    //  Serial NO - 6 and lastly it adds whatever is in global object. so the color change one more thing it also has one dependency.
     htmlClasslist.add(themeMode);
   }, [themeMode]);
 
 
   return (
+    //  Serial NO - 2 By giving the value={{ themeMode, lightTheme, darkTheme }} We are telling the app.jsx use this info to write the logic which is just above in line no 9.
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
