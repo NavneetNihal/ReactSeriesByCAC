53e23ba748362464f25d4a18a6202dd4cd36afdc
Navneet Nihal <lakranihal0070@gmail.com>
2026-04-13 00:09:35 +0530

    Megablog-appwrite12

--- megablog-apwrite12/src/components/Header/Header.jsx
+++ megablog-apwrite12/src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Container, Logo, LogoutBtn } from '../index'
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
-  const authStatus = useSelector((state) =>
-    state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
 
   const navItems = [
@@ -37,25 +36,27 @@ function Header() {
     },
   ]
 
+
   return (
     <header className='py-3 shadow bg-gray-500'>
       <Container>
         <nav className='flex'>
           <div className='mr-4'>
             <Link to='/'>
-            <Logo width='70px' />
+              <Logo width='70px' />
+
             </Link>
           </div>
           <ul className='flex ml-auto'>
             {navItems.map((item) =>
-            item.active ? (
-              <li key={item.name}>
-                <button
-                onClick= {() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                >{item.name}</button>
-              </li>
-            ) : null 
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >{item.name}</button>
+                </li>
+              ) : null
             )}
             {authStatus && (
               <li>
