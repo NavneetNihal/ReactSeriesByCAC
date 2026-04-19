e2e193a579f6e5cecbbbd3cdea1343491401d25a
Navneet Nihal <lakranihal0070@gmail.com>
2026-04-19 18:20:19 +0530

    interview-discussion

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -6,22 +6,30 @@ import './App.css'
 
 function App() {
   const [value, setValue] = useState(1)
+
+
+
   // const [multipliedvalue, setMultipliedvalue] = useState(1)
 
-  let multipliedvalue = value * 5
+  // let multipliedvalue = value * 5
 
-  const multlipliesbyFive = () => {
-    // setMultipliedvalue(value * 5)
-    setValue(value + 1)
+  // const multlipliesbyFive = () => {
+  //   // setMultipliedvalue(value * 5)
+  //   setValue(value + 1)
+  // }
+
+  const clickMe = () => {
+    console.log("logged");
   }
+
 
   return (
     <>
     <h1>Main value: {value}</h1>
     <button
-    onClick={multlipliesbyFive}
+    onClick={clickMe}
     >Click to multiply by 5</button>
-    <h2>Multiplied value: {multipliedvalue} </h2>
+    {/* <h2>Multiplied value: {multipliedvalue} </h2> */}
     </>
   )
 }
